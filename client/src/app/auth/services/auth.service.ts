4a46cf4ec956f4c67a74cbb9d26f5a8f44cc2a18
hodattadoh <127475009+hodattadoh@users.noreply.github.com>
2023-07-12 23:38:07 +0700

    Add files via upload

---
+++ client/src/app/auth/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, filter, map, Observable } from 'rxjs';
+import { CurrentUserInterface } from '../types/currentUser.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
+import { LoginRequestInterface } from '../types/loginRequest.interface';
+
+@Injectable()
+export class AuthService {
+  currentUser$ = new BehaviorSubject<CurrentUserInterface | null | undefined>(
+    undefined
+  );
+  isLogged$ = this.currentUser$.pipe(
+    filter((currentUser) => currentUser !== undefined),
+    map(Boolean)
+  );
+
+  constructor(private http: HttpClient) {}
+
+  getCurrentUser(): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/user';
+    return this.http.get<CurrentUserInterface>(url);
+  }
+
+  register(
+    registerRequest: RegisterRequestInterface
+  ): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users';
+    return this.http.post<CurrentUserInterface>(url, registerRequest);
+  }
+
+  login(loginRequest: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login';
+    return this.http.post<CurrentUserInterface>(url, loginRequest);
+  }
+
+  setToken(currentUser: CurrentUserInterface): void {
+    localStorage.setItem('token', currentUser.token);
+  }
+
+  setCurrentUser(currentUser: CurrentUserInterface | null): void {
+    this.currentUser$.next(currentUser);
+  }
+}
